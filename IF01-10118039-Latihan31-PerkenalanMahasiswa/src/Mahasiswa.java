bb3ed33e713f6f20ebaa4f127214f54a5150faa7
Fatahillah Satria Bima Seno <55565303+fathsbs@users.noreply.github.com>
2019-10-13 15:49:42 +0700

    Add files via upload

---
+++ IF01-10118039-Latihan31-PerkenalanMahasiswa/src/Mahasiswa.java
@@ -0,0 +1,24 @@
+/*
+ * NAMA              : FATAHILLAH SATRIA BIMA SENO
+ * KELAS             : IF-01
+ * NIM               : 10118039
+ * DESKRIPSI PROGRAM : Program Biodata  
+ */
+
+/**
+ *
+ * @author Fatahillah Seno
+ */
+public class Mahasiswa {
+
+    public String nim;
+    public String nama;
+
+    public void perkenalkanDiri(String nim, String nama) {
+        System.out.println("Hallo Everyone");
+        System.out.printf("My NIM is\t: %s%n", nim);
+        System.out.printf("My Name is\t: %s%n%n", nama);
+
+    }
+
+}
---
+++ IF01-10118039-Latihan31-PerkenalanMahasiswa/src/Main.java
@@ -0,0 +1,19 @@
+/*
+ * NAMA              : FATAHILLAH SATRIA BIMA SENO
+ * KELAS             : IF-01
+ * NIM               : 10118039
+ * DESKRIPSI PROGRAM : Program Biodata   
+ */
+
+public class Main {
+
+    public static void main(String[] args) {
+        Mahasiswa intro = new Mahasiswa();
+        intro.perkenalkanDiri("10118039", "Fatahillah Satria Bima Seno");
+        intro.perkenalkanDiri("10118037", "Muhammad Ihsan");
+        intro.perkenalkanDiri("10118043", "Dian Rosa Pratama");
+        intro.perkenalkanDiri("10118009", "Tassyakur Pasya");
+
+    }
+
+}
